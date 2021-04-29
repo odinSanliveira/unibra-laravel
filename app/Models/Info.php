2fde7ebc638fa8bdf2a7c02730d2bdb0b7e7fb78
odinSanliveira <odin.sanliveira@gmail.com>
2021-04-29 19:05:43 -0300

    adicionando controller e model de info

---
+++ app/Models/Info.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Info extends Model
+{
+    protected $fillable = [
+        'cnpj',
+        'nome',
+        'razaoSocial',
+        'data_abertura',
+        'capital_social',
+        'tipo',
+        'nat_juridica',
+        'porte',
+        'qtdFuncionarios',
+        'faturamento',
+        'telefone',
+        'email',
+        'estado',
+        'municipio',
+        'bairro',
+        'logradouro',
+        'numero',
+        'complemento',
+        'cep',
+        'Atv_economica'
+        
+    ];
+}
