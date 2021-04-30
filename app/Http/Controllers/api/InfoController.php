2c28cb19bbb1b464b7e4f23e73b8c4a0ca3ad129
odinSanliveira <odin.sanliveira@gmail.com>
2021-04-30 10:15:03 -0300

    api retornando dados em json

--- app/Http/Controllers/api/InfoController.php
+++ app/Http/Controllers/api/InfoController.php
@@ -4,80 +4,55 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Info;
 
 class InfoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+{   
+    protected $objInfo;
+    //criando objeto para teste
+    public function __construct(){
+        $this->objInfo = new Info();
+    }
+    /* Mantendo todas as funções para possível uso futuro */
     public function index()
-    {
-        //
+    {   //retorna todas as informações do banco de dados;
+        return Info::all();
+
+        //dd -> função var_dump laravel
+        //usando para retornar dados do bd
+    //    dd($this->objInfo->all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
-    {
-        //
+    {   //possibilita a introdução de valores nas tabelas da database;
+        Info::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         //
